b74ec0381477845ec0edc3f33d20e5950d1d5826
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-06 05:47:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,8 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(namelist)
-  namelist = []
+def batch_badge_creator(speaker)
+  speaker = [Esdger, Ada, Charles, Alan]
   
+  
+def assign_room
