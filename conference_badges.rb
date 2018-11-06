fb1a84f318314e3770beba99b125a5d64e9bb86f
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-06 06:03:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(speakers)
   end
 end
   
-def assign_room(speakers)
+def assign_rooms(speakers)
   speakers.each_with_index.map do |speaker, index|
     "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
